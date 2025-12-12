ea59e80c12f42fb3555d499f16e496f23512ccb0
Pannnomaly <supawith007@gmail.com>
2025-12-12 19:21:24 +0700

    hotfix: shadow in admin table

--- src/components/DataTableAdmin.jsx
+++ src/components/DataTableAdmin.jsx
@@ -3,8 +3,8 @@ import ToggleButton from "./ToggleButton";
 export default function DataTableAdmin ()
 {
     return (
-        <div className="flex justify-center items-center mt-20 shadow-md">
-            <table className="text-center text-xl">
+        <div className="flex justify-center items-center mt-20">
+            <table className="text-center text-xl shadow-md">
                 <thead>
                     <tr className="bg-pink-200">
                         <th className="border px-30 py-4">Name</th>
